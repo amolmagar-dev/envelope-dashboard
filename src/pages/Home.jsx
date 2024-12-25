113068ce6f1b1ce8a005db8bea157c85e1df9082
Amol Magar <amolmagar.dev@gmail.com>
2024-12-25 17:01:44 +0530

    fix: Remove unnecessary z-index from sticky header in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -289,7 +289,7 @@ const Home = () => {
                 {selectedEmail ? (
                     <>
                         {/* Sticky Header Section */}
-                        <div className="sticky top-0 bg-white z-10">
+                        <div className="sticky top-0 bg-white">
                             {/* Toolbar */}
                             <div className="h-12 bg-gray-50 border-b border-gray-200 flex items-center px-4 justify-between">
                                 <div className="flex space-x-2">
