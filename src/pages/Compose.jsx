20e2f1e1159fd71a2392b4afa49eda872d36f199
Amol Magar <amolmagar.dev@gmail.com>
2024-12-25 15:19:35 +0530

    feat: Make footer sticky in ComposeModal when maximized

--- src/pages/Compose.jsx
+++ src/pages/Compose.jsx
@@ -151,7 +151,10 @@ const ComposeModal = ({ isOpen, onClose }) => {
                     </div>
 
                     {/* Footer */}
-                    <div className="px-4 py-2 border-t bg-gray-50 flex items-center justify-between">
+                    <div
+                        className={`px-4 py-2 border-t bg-gray-50 flex items-center justify-between ${isMaximized ? 'sticky bottom-0' : ''
+                            }`}
+                    >
                         <div className="flex items-center space-x-2">
                             <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors flex items-center">
                                 <Send className="w-4 h-4 mr-2" />
